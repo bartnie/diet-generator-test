d0af629d77f379c156ce722ccc73bcc199e50d47
Niesobski, Bartosz <bartosz.niesobski@sap.com>
2018-05-13 15:18:34 +0200

    ingredient service generated

---
+++ ingredient/src/main/java/pl/bartek/ingredient/IngredientApplication.java
@@ -0,0 +1,12 @@
+package pl.bartek.ingredient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngredientApplication.class, args);
+	}
+}
